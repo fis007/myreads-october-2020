6c1e51957339d0d4a0128de7a3ac1f9b3d28eca7
fis007 <schaffhauserfilip@gmail.com>
2020-10-24 12:39:55 +0800

    created components for each function

---
+++ src/Book.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+
+class Book extends Component {
+  render() {
+    return (
+      <li>
+        <div className="book">
+          <div className="book-top">
+            <div
+              className="book-cover"
+              style={{
+                width: 128,
+                height: 193,
+                backgroundImage: 'url("")',
+              }}
+            />
+            <div className="book-shelf-changer">
+              <select>
+                <option value="move" disabled>
+                  Move to...
+                </option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+              </select>
+            </div>
+          </div>
+          <div className="book-title">PROPS.TITLE</div>
+          <div className="book-authors">PROPS.AUTHOR</div>
+        </div>
+      </li>
+    );
+  }
+}
+
+export default Book;
---
+++ src/Main.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import Shelf from "./Shelf";
+
+class Main extends Component {
+  render() {
+    return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            <Shelf shelfName="Currently Reading" />
+            <Shelf shelfName="Want to Read" />
+            <Shelf shelfName="Read" />
+          </div>
+        </div>
+        <div className="open-search">
+          <button onClick={() => this.setState({ showSearchPage: true })}>
+            Add a book
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Main;
